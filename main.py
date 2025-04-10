4e96a1a840043d77a4cacadcc71dad7936ade8c9
Arsh Mittal <arshmittalsitl@gmail.com>
2025-04-11 00:09:27 +0530

    port

--- main.py
+++ main.py
@@ -1192,4 +1192,5 @@ def get_question_votes(question):
     })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
